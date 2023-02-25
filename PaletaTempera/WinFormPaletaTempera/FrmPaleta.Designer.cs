4e51497e2c093309895aca5a458655e6407d575f
juanCrMendez <juancruzmendezz.13@gmail.com>
2023-02-25 20:53:30 -0300

    25-02-2023

---
+++ PaletaTempera/WinFormPaletaTempera/FrmPaleta.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WinFormPaletaTempera
+{
+    partial class FrmPaleta
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtVisor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAgregar.Location = new System.Drawing.Point(197, 307);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(175, 35);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar Tempera";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtVisor
+            // 
+            this.txtVisor.Location = new System.Drawing.Point(12, 12);
+            this.txtVisor.Multiline = true;
+            this.txtVisor.Name = "txtVisor";
+            this.txtVisor.Size = new System.Drawing.Size(544, 270);
+            this.txtVisor.TabIndex = 1;
+            // 
+            // FrmPaleta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 354);
+            this.Controls.Add(this.txtVisor);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "FrmPaleta";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtVisor;
+    }
+}
+
